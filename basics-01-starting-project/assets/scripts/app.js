c084d3eff9617ce687ecb48ef2494233c37068c9
Stefan Ossanna <st.ossanna@gmail.com>
2024-07-13 15:26:40 +0200

    assignment 1 added

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,7 @@
+let currentResult = 0;
+
+currentResult += 10;
+
+let calcDescription = `(${currentResult} + 10)  * 3`;
+
+outputResult(currentResult, calcDescription);
---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,8 @@
+let fictionalInput = 25;
+let result = 18;
+result += fictionalInput;
+result -= 4;
+result /= 2;
+result *= 5;
+alert(result);
+alert(fictionalInput);
