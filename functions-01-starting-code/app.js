af0bc803a5a2612c7920c745e52466e7e191a4cf
Stefan Ossanna <st.ossanna@gmail.com>
2024-07-19 19:44:07 +0200

    added function01 project

---
+++ functions-01-starting-code/app.js
@@ -0,0 +1,64 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
+
+let gameIsRunning = false;
+let gameLog = [];
+
+const getPlayerChoice = function () {
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
+    if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+        alert('Invalid choice! We chose Rock for you!');
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection;
+}
+
+const getComputerChoice = function () {
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+        return ROCK;
+    } else if (randomValue < 0.67) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+}
+
+// alert(`It's a draw!`);
+
+const getWinner = function (pChoice, cChoice) {
+    gameIsRunning = false;
+    if (pChoice === cChoice) {
+        return RESULT_DRAW;
+    } else if (cChoice === ROCK && pChoice === PAPER || cChoice === PAPER && pChoice === SCISSORS || cChoice === SCISSORS && pChoice === ROCK) {
+        return RESULT_PLAYER_WINS;
+    } else {
+        return RESULT_COMPUTER_WINS;
+    }
+}
+
+const addEntryLog = function(pChoice, cChoice, winner) {
+    gameLog.push({
+        Player_Choice: pChoice,
+        Computer_Choice: cChoice,
+        Round_Winner: winner
+    })
+    return gameLog;
+}
+
+startGameBtn.addEventListener('click', () => {
+    if (gameIsRunning) return;
+    gameIsRunning = true;
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    const winner = getWinner(playerSelection, computerSelection);
+    const gameLog = addEntryLog(playerSelection, computerSelection, winner)
+    console.log(gameLog)
+});
