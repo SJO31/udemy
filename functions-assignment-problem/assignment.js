62b8acb9563d4e50932b733fcd9af4e86e333997
Stefan Ossanna <st.ossanna@gmail.com>
2024-07-20 13:15:01 +0200

    finished assignment

--- functions-assignment-problem/assignment.js
+++ functions-assignment-problem/assignment.js
@@ -1,5 +1,37 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
+const nameInput1 = (salutation = 'Dear', name = 'Sir') => {
+  let greeting;
+  if (salutation && name) {
+    console.log(`${salutation} ${name}`);
+    greeting = `${salutation} ${name}. What a great day!`
+  } else {
+    console.log(`Hi Joe`);
+  }
+  return greeting;
+}
+const nameInput2 = (name, phrase) => console.log(`${phrase} ${name}`);
+const nameInput3 = () => console.log(`Dear Sir`);
+const nameInput4 = name => `Dear ${name}`;
+const nameInput5 = (name, phrase = 'Dear') => console.log(`${phrase} ${name}`);
+
+nameInput1(undefined, 'John');
+nameInput2('Hello', 'John');
+nameInput3();
+console.log(nameInput4('Hello', 'John'));
+
+function checkInput(cb, ...arg) {
+  let hasEmptyText = false;
+  for (const text of arg) {
+    if (!text) {
+      hasEmptyText = true;
+      break;
+    }
+  }
+  if (!hasEmptyText) {
+    cb();
+  }
 }
 
-sayHello();
+checkInput(() => {
+  console.log('All not empty')
+}, 'hi', '142', 'asdf', 'sdf')
+
