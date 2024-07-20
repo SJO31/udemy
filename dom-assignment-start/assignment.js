3131c676da3c3d17e669b0644427bb80820ab49f
Stefan Ossanna <st.ossanna@gmail.com>
2024-07-20 19:24:55 +0200

    solved dom assignment

---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,14 @@
+let task1_b = document.getElementById('task-1');
+let task1_a = document.querySelector('#task-1');
+
+task1_a.style.color = 'white';
+task1_b.style.backgroundColor = 'black';
+
+let task2_a = document.querySelector('title');
+let task2_b = document.head.querySelector('title');
+
+task2_a.textContent = 'Assignment - Solved!'
+
+let task3 = document.getElementsByTagName('h1');
+
+task3[0].textContent = 'Assignment - Solved!'
