51f0e8d048afbaf6a479d45900cff59f333f8478
Stefan Ossanna <st.ossanna@gmail.com>
2024-07-26 15:51:56 +0200

    added array functions

---
+++ arrays-01-starting-code/app.js
@@ -0,0 +1,11 @@
+// Basic Array
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+// Array Function; This array method is slower as brackets!
+const moreNumbers = new Array(5); // creates an array of length 5, NOT with the element 5
+console.log(moreNumbers);
+
+// This array method is slower as brackets!
+const moreMoreNumbers = Array.of(1, 2);
+console.log(moreMoreNumbers);
