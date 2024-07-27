8c9ec4b7193ffb2334899dbd4a068763b6cae36f
Stefan Ossanna <st.ossanna@gmail.com>
2024-07-27 19:45:37 +0200

    tested maps

--- arrays-01-starting-code/setsmaps.js
+++ arrays-01-starting-code/setsmaps.js
@@ -1,13 +1,34 @@
-const ids = new Set(['Good','Evening','Sir']);
-ids.add(2);
+// const ids = new Set(['Good','Evening','Sir']);
+// ids.add(2);
 
-for (const entry of ids.entries()) {
+// for (const entry of ids.entries()) {
+//     console.log(entry);
+// }
+
+// for (const value of ids.values()) {
+//     console.log(value);
+    
+// }
+
+// console.log(ids);
+
+const person1 = {name: 'Max'};
+const person2 = {name: 'Laura'};
+
+const personData = new Map([[person1, [{date: '2024-08-23', price: 10}]]]);
+
+personData.set(person2, [{date: '2024-01-22', price: 44}]);
+
+console.log(personData);
+console.log(personData.get(person1));
+
+for ( const entry of personData.entries()) {
     console.log(entry);
-}
-
-for (const value of ids.values()) {
-    console.log(value);
     
 }
 
-console.log(ids);
+for (const key of personData.keys()) {
+    console.log(key);
+    
+}
+console.log(personData.size);
