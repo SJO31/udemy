0a9589f97f826fb8ae9ab9f92b46fb72ed92c14c
Stefan Ossanna <st.ossanna@gmail.com>
2024-08-03 10:50:04 +0200

    added productList class

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,8 +1,8 @@
 class Product {
-    title = "DEFAULT";
-    imageUrl;
-    price;
-    description;
+    // title = "DEFAULT";
+    // imageUrl;
+    // price;
+    // description;
 
     constructor(title, img, price, desc) {
         this.title = title;
@@ -12,8 +12,31 @@ class Product {
     }
 }
 
-const productList = {
-    products: [
+class ProductItem {
+    constructor(product) {
+        this.product = product;
+    }
+
+    render() {
+        const prodEl = document.createElement("li");
+        prodEl.className = "product-item";
+        prodEl.innerHTML = `
+            <div>
+                <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+                <div class="product-item__content">
+                    <h2>${this.product.title}</h2>
+                    <h3>\$${this.product.price}</h3>
+                    <p>${this.product.description}</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+        `;
+        return prodEl;
+    }
+}
+
+class ProductList {
+    products = [
         new Product(
             "Pillow",
             "https://images.unsplash.com/photo-1534628123120-3235d5a13277?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -26,29 +49,21 @@ const productList = {
             39.99,
             "A robust indoor carpet"
         )
-    ],
+    ];
+
+    constructor() {};
     render() {
         const renderHook = document.getElementById("app");
         const prodList = document.createElement("ul");
         prodList.className = "product-list";
         for (const prod of this.products) {
-            const prodEl = document.createElement("li");
-            prodEl.className = "product-item";
-            prodEl.innerHTML = `
-                <div>
-                    <img src="${prod.imageUrl}" alt="${prod.title}" >
-                    <div class="product-item__content">
-                        <h2>${prod.title}</h2>
-                        <h3>\$${prod.price}</h3>
-                        <p>${prod.description}</p>
-                        <button>Add to Cart</button>
-                    </div>
-                </div>
-            `;
+            const productItem = new ProductItem(prod);
+            const prodEl = productItem.render();
             prodList.append(prodEl);
         }
         renderHook.append(prodList);
-    },
-};
+    };
+}
 
+const productList = new ProductList();
 productList.render();
