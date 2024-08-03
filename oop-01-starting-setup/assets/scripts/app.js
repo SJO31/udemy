6b71cf482dde3de532f0c8c2e90d4926862583bf
Stefan Ossanna <st.ossanna@gmail.com>
2024-08-03 10:17:04 +0200

    added product list object with render function

---
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,40 @@
+const productList = {
+    products: [
+        {
+            title: "Pillow",
+            imageUrl: "https://images.unsplash.com/photo-1534628123120-3235d5a13277?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            price: 19.99,
+            description: "A soft pillow!",
+        },
+        {
+            title: "Carpet",
+            imageUrl: "https://images.unsplash.com/photo-1444362408440-274ecb6fc730?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            price: 39.99,
+            description: "A robust indoor carpet",
+        },
+    ],
+    render() {
+        const renderHook = document.getElementById('app');
+        const prodList = document.createElement('ul');
+        prodList.className = 'product-list';
+        for (const prod of this.products) {
+            const prodEl = document.createElement('li');
+            prodEl.className = 'product-item';
+            prodEl.innerHTML = `
+                <div>
+                    <img src="${prod.imageUrl}" alt="${prod.title}" >
+                    <div class="product-item__content">
+                        <h2>${prod.title}</h2>
+                        <h3>\$${prod.price}</h3>
+                        <p>${prod.description}</p>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
+            `;
+            prodList.append(prodEl);
+        }
+        renderHook.append(prodList);
+    }
+}
+
+productList.render();
