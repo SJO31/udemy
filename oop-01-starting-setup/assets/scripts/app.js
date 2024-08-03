f7a83c20fccaaa1078fe7f1cc5ae4bb08b4cc47b
Stefan Ossanna <st.ossanna@gmail.com>
2024-08-03 11:49:13 +0200

    adding getters & setters

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -15,9 +15,22 @@ class Product {
 class ShoppingCart {
     items = [];
 
+    set cartItems(value) {
+        this.items = value;
+        this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(2)}</h2>`
+    }
+
+    get totalAmount() {
+        const sum = this.items.reduce((prevValue, curItem) => {
+            return prevValue + curItem.price;
+        }, 0);
+        return sum;
+    }
+
     addProduct(product) {
-        this.items.push(product);
-        this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`
+        const updateItems = [...this.items];
+        updateItems.push(product);
+        this.cartItems = updateItems;
     }
 
     render() {
@@ -106,7 +119,7 @@ class Shop {
 
 class App {
     static cart;
-    
+
     static init() {
         const shop = new Shop();
         shop.render();
