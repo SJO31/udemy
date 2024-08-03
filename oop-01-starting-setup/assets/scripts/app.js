9310b47c1d1d32da0da22d170e665051f2a14070
Stefan Ossanna <st.ossanna@gmail.com>
2024-08-03 10:25:53 +0200

    added product class

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,25 +1,39 @@
+class Product {
+    title = "DEFAULT";
+    imageUrl;
+    price;
+    description;
+
+    constructor(title, img, price, desc) {
+        this.title = title;
+        this.imageUrl = img;
+        this.price = price;
+        this.description = desc;
+    }
+}
+
 const productList = {
     products: [
-        {
-            title: "Pillow",
-            imageUrl: "https://images.unsplash.com/photo-1534628123120-3235d5a13277?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            price: 19.99,
-            description: "A soft pillow!",
-        },
-        {
-            title: "Carpet",
-            imageUrl: "https://images.unsplash.com/photo-1444362408440-274ecb6fc730?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            price: 39.99,
-            description: "A robust indoor carpet",
-        },
+        new Product(
+            "Pillow",
+            "https://images.unsplash.com/photo-1534628123120-3235d5a13277?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            19.99,
+            "A soft pillow!"
+        ),
+        new Product(
+            "Carpet",
+            "https://images.unsplash.com/photo-1444362408440-274ecb6fc730?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            39.99,
+            "A robust indoor carpet"
+        )
     ],
     render() {
-        const renderHook = document.getElementById('app');
-        const prodList = document.createElement('ul');
-        prodList.className = 'product-list';
+        const renderHook = document.getElementById("app");
+        const prodList = document.createElement("ul");
+        prodList.className = "product-list";
         for (const prod of this.products) {
-            const prodEl = document.createElement('li');
-            prodEl.className = 'product-item';
+            const prodEl = document.createElement("li");
+            prodEl.className = "product-item";
             prodEl.innerHTML = `
                 <div>
                     <img src="${prod.imageUrl}" alt="${prod.title}" >
@@ -34,7 +48,7 @@ const productList = {
             prodList.append(prodEl);
         }
         renderHook.append(prodList);
-    }
-}
+    },
+};
 
 productList.render();
