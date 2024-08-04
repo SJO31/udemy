a8c2e4ebe74c4cc94639a225b2b30ecf207cf008
Stefan Ossanna <st.ossanna@gmail.com>
2024-08-04 12:16:25 +0200

    change function from public to private

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -20,9 +20,14 @@ class ElementAttribute {
 }
 
 class Component {
-    constructor(renderHookId) {
+    constructor(renderHookId, shouldRender = true) {
         this.hookId = renderHookId;
+        if (shouldRender) {
+            this.render();
+        }
     }
+
+    render() {};
 
     createRootElement(tag, cssClasses, attributes) {
         const rootElement = document.createElement(tag);
@@ -64,21 +69,28 @@ class ShoppingCart extends Component {
         this.cartItems = updateItems;
     }
 
+    orderProducts() {
+        console.log('Ordering...');
+        console.log(this.items);
+    }
+
     render() {
         const cartEl = this.createRootElement('section', 'cart');
         cartEl.innerHTML = `
             <h2>Total: \$${0}</h2>
             <button>Order Now!</button>
         `;
+        const orderButton = cartEl.querySelector('button');
+        orderButton.addEventListener('click', this.orderProducts.bind(this));
         this.totalOutput = cartEl.querySelector('h2');
-        return cartEl;
     }
 }
 
 class ProductItem extends Component{
     constructor(product, renderHookId) {
-        super(renderHookId);
+        super(renderHookId, false);
         this.product = product;
+        this.render();
     }
 
     addCartHandler() {
@@ -105,40 +117,53 @@ class ProductItem extends Component{
 }
 
 class ProductList extends Component{
-    products = [
-        new Product(
-            "Pillow",
-            "https://images.unsplash.com/photo-1534628123120-3235d5a13277?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            19.99,
-            "A soft pillow!"
-        ),
-        new Product(
-            "Carpet",
-            "https://images.unsplash.com/photo-1444362408440-274ecb6fc730?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            39.99,
-            "A robust indoor carpet"
-        )
-    ];
+    products = [];
 
     constructor(renderHookId) {
         super(renderHookId);
+        this.#fetchProducts();
     };
 
-    render() {
-        const prodList = this.createRootElement('ul', 'product-list', [new ElementAttribute('id', 'prod-list')])
+    #fetchProducts() { // marked as a private function. works with fields as well
+        this.products = [
+            new Product(
+                "Pillow",
+                "https://images.unsplash.com/photo-1534628123120-3235d5a13277?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                19.99,
+                "A soft pillow!"
+            ),
+            new Product(
+                "Carpet",
+                "https://images.unsplash.com/photo-1444362408440-274ecb6fc730?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                39.99,
+                "A robust indoor carpet"
+            )
+        ];
+        this.render();
+    }
+
+    renderProducts() {
         for (const prod of this.products) {
-            const productItem = new ProductItem(prod, 'prod-list');
-            productItem.render();
+            new ProductItem(prod, 'prod-list');
+        }
+    }
+
+    render() {
+        this.createRootElement('ul', 'product-list', [new ElementAttribute('id', 'prod-list')]);
+        if (this.products && this.products.length > 0) {
+            this.renderProducts();
         }
     };
 }
 
 class Shop {
+    constructor() {
+        this.render();
+    }
+
     render() {
         this.cart = new ShoppingCart('app');
-        this.cart.render();
-        const productList = new ProductList('app');
-        productList.render();
+        new ProductList('app');
     }
 }
 
@@ -147,7 +172,6 @@ class App {
 
     static init() {
         const shop = new Shop();
-        shop.render();
         this.cart = shop.cart;
     }
 
