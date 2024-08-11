3a711634c307359edf337612104da39ae5669b7d
Stefan Ossanna <st.ossanna@gmail.com>
2024-08-11 14:36:21 +0200

    added event.js file

---
+++ S12_practice-oop-01-starting-setup/assets/scripts/events.js
@@ -0,0 +1,17 @@
+const buttons = document.querySelectorAll('button');
+
+const buttonClickHandler = event => {
+    // event.target.disabled = true;
+    console.log(event);
+}
+
+const anotherButtonClickHandler = () => {
+    console.log('This was clicked!');
+}
+
+const boundFn = buttonClickHandler.bind(this);
+
+buttons.forEach(btn => {
+    // btn.addEventListener('click', buttonClickHandler);
+    // btn.addEventListener('mouseenter', buttonClickHandler);
+});
